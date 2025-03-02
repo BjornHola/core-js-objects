d6be626d6a9b0f34d95d641fb5cf73d6e4d1a4e2
BjornHola <broadherstby@gmail.com>
2025-03-02 22:30:25 +0300

    init: fulfill task rectangle

--- src/objects-tasks.js
+++ src/objects-tasks.js
@@ -237,10 +237,19 @@ sellTickets([25, 100]);
  *    console.log(r.height);      // => 20
  *    console.log(r.getArea());   // => 200
  */
-function Rectangle(/* width, height */) {
-  throw new Error('Not implemented');
+function Rectangle(width, height) {
+  return {
+    width,
+    height,
+    getArea() {
+      return this.width * this.height;
+    },
+  };
 }
-
+const r = new Rectangle(10, 20);
+console.log(r.width);
+console.log(r.height);
+console.log(r.getArea());
 /**
  * Returns the JSON representation of specified object
  *
